37f63d961ea61f76d90878f9810403599ac284aa
Arun S <arunksat@hotmail.com>
2015-10-03 10:55:47 -0700

    Cleaning up comments.

--- Test-Aug.py
+++ Test-Aug.py
@@ -1,28 +1,14 @@
 
 __author__ = 'gamargam'
 
-# first file
 import json
 import sys
-import scrapy
 import prettytable
 
-
-print "Hello world"
-
-json_string = '{"first_name": "Guido", "last_name":"Rossum"}'
-new_dict1 = {"first_name1": "Guido1", "Last_name1": "Rossum1"}
-
-# json_string = '{ "away": "TEN","day": 3,"eid": "2016010305","gamekey": "56752","home": "IND","month": 1,"season_type": "REG","time": "1:00","wday": "Sun","week": 17,"year": 2015}'
-parsed_json = json.loads(json_string)
-print(parsed_json['first_name'])
-print(new_dict1["first_name1"])
 global home_team
 global away_team
 
-# Week 1 schedule - Aug 11
-
-
+'''
 week1_games = {"game1_away": "PIT", "game1_home": "NE", "game2_away": "GB", "game2_home": "CHI",
                "game3_away": "KC", "game3_home": "HOU", "game4_away": "CLE", "game4_home": "NYJ",
                "game5_away": "IND", "game5_home": "BUF", "game6_away": "MIA", "game6_home": "WAS",
@@ -32,19 +18,11 @@ week1_games = {"game1_away": "PIT", "game1_home": "NE", "game2_away": "GB", "gam
                "game13_away": "BAL", "game13_home": "DEN", "game14_away": "NYG", "game14_home": "DAL",
                "game15_away": "PHI", "game15_home": "ATL", "game16_away": "MIN", "game16_home": "SF"
                }
-
 '''
+
 week1_games = {"game1_away": "PIT", "game1_home": "NE", "game2_away": "GB", "game2_home": "CHI",
                "game3_away": "KC", "game3_home": "HOU", "game4_away": "CLE", "game4_home": "NYJ",
                }
-'''
-
-# print(week1_games["game16_home"])
-
-# Display formatting - Aug 12
-# print(sorted(week1_games.keys()))
-# print(len(week1_games))
-# print(week1_games.values())
 
 # Comments:
 # start for loop
@@ -74,15 +52,9 @@ for key_val in sorted(week1_games.iterkeys()):  # i = week1_games[key_val]
     elif i == 2:
         home_team = week1_games[key_val]
         i = 1
-
-        #print ("| {} | v/s | {} |".format(away_team, home_team))
-        #print '----------------'
-
         table.add_row([away_team, home_team])
 
 print table
-
-# Aug 25 - No clean-up attempted. Still struggling with basics on Input, Display
 
 home_list = []
 away_list = []
@@ -98,19 +70,9 @@ for key_val in sorted(week1_games.iterkeys()):  # i = week1_games[key_val]
     elif i == 2:
         home_team = week1_games[key_val]
         i = 1
-
-        #print ' %s \t v/s \t %s \n' % (away_team, home_team)
-        # Attempting str.format() instead of % string formatting literal - Aug 18
-        #print ("| {} | v/s | {} |".format(away_team, home_team))
-        #print '----------------'
-        #print "Enter Home Team's score:  " - Aug 19
-        #print "Enter %s (home team) projected score" % home_team - Aug 26
-
         counter += 1
-        #print "\n Game # ", counter
-        #print "============"
-
         print("\n Enter home team score {} ".format(home_team)),
+
         # Exception handling required for input function
         try:
             temp_home_score = int(input(": "))
@@ -132,14 +94,6 @@ for key_val in sorted(week1_games.iterkeys()):  # i = week1_games[key_val]
 
 
 print '\n'
-
 print score_table
-
-#f_project.write(score_table)
-
 print "Work In Progress...\n"
-
-#print home_list
-#print away_list
-
 print (sys.version)
--- scrape-new.py
+++ scrape-new.py
@@ -3,7 +3,6 @@ __author__ = 'gamargam'
 
 
 import requests
-#from BeautifulSoup import BeautifulSoup
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 
@@ -14,7 +13,7 @@ url_reg = 'http://www.nfl.com/scores/2015/REG1'
 #html = response.content
 #soup = BeautifulSoup(html, "lxml")
 
-soup = BeautifulSoup(open("week1-scores.html").read(), "lxml")
+soup = BeautifulSoup(open("week10-scores.html").read(), "lxml")
 print type(soup)
 soup.prettify()
 print soup.title.string
@@ -43,115 +42,3 @@ for link_val1, link_val2 in zip(link_away, link_home):
 print score_table
 #f_project.write(score_table)
 
-'''
-#sibling_soup.b.next_sibling
-
-# Testing code.
-
-#for td in soup.select('div class > p class > p class:nth-of-type(2)'):
-#    print td.text
-#    print "hello inside"
-
-
-#table = soup.find('div', attrs={'class': 'scorebox-wrapper'}) #this statement works
-#table = soup.findAll(['div', 'p ']) # this also works
-#table = soup.findAll(['div', 'p'])
-#table = soup.find_all('div', class_='scorebox-wrapper')
-
-#print type(table)
-#print table
-
-#manga_img = soup.findAll ('a', {'class' : 'manga_img'}, limit=None)
-#2 for loops: one for team name and one for total-score
-
-#for row in soup.find_all("p", attrs={"class": "team-name"}):
-#    print row.prettify()
-
-#for row in soup.findAll('p', {'class': 'team-name'}, limit=None): #prints scores alone# #
-#   print row.prettify()
-
-#rank = soup.find("div", {"class": "team-name"}).p.contents
-
-
-#teaminfo = soup.find("p", {"class": "team-name"}).contents
-#score = soup.find("p", {"class": "total-score"}).contents
-#print teaminfo
-#print score
-
-#rating = teaminfo.p.contents
-#print rating
-
-
-#for row in soup.findAll(True, {"class":["team-name", "total-score"]}):
-#    print row.prettify()
-
-total_score = len(soup.findAll(True, {"class" : ["team-name"]}))
-print int(total_score)
-
-for num in [2,3]:
-    print soup.select('.team-name')[num].contents[0]
-    print soup.select('.total-score')[num].contents[0]
-
-
-#for title in soup.findAll('p', {'class': 'team-name'}, limit=None):
-#    print title
-
-count = 0
-
-
-link = soup.findAll('div', {"class" : ['home-team']})
-
-for link_val in link:
-    divlinks = link_val.findAll('p', {'class' : 'team-name'})
-    for divlink_val in divlinks:
-        team_url = divlink_val.find('a', href=True)['href']
-        print team_url
-
-print "+++++++++++++++++"
-print "-----------------"
-print "+++++++++++++++++"
-
-
-linknew = soup.findAll('div', {"class" : ['home-team']})
-
-for link_newval in linknew:
-    divnewlinks = link_newval.findAll('p', {'class' : 'total-score'})
-    print divnewlinks
-
-print "+++++++++++++++++"
-print "-----------------"
-print "+++++++++++++++++"
-
-link = soup.findAll('div', {"class" : ['away-team']})
-
-for link_val in link:
-    divlinks = link_val.findAll('p', {'class' : 'team-name'})
-    for divlink_val in divlinks:
-        team_url = divlink_val.find('a', href=True)['href']
-        print team_url
-
-print "+++++++++++++++++"
-print "-----------------"
-print "+++++++++++++++++"
-
-
-linknew = soup.findAll('div', {"class" : ['away-team']})
-
-for link_newval in linknew:
-    divnewlinks = link_newval.findAll('p', {'class' : 'total-score'})
-    print divnewlinks
-
-print "+++++++++++++++++"
-print "-----------------"
-print "+++++++++++++++++"
-
-
-scores = soup.select('.total-score')
-print scores
-
-print scores[1].contents[0]
-print scores[2].contents[0]
-print scores[4].contents[0]
-print scores[5].contents[0]
-
-'''
